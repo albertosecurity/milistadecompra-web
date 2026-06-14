40c4b64aa7af6a483fb0a2c8eff670b5c3c8d5bd
albertosecurity <alberto.cibersec@gmail.com>
2026-06-14 19:32:28 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useAuth } from './context/AuthContext'
+import { ThemeProvider } from './context/ThemeContext'
 import LoginPage from './pages/LoginPage'
 import ListsPage from './pages/ListsPage'
 import ListDetailPage from './pages/ListDetailPage'
 import ProductsPage from './pages/ProductsPage'
 import PendingPage from './pages/PendingPage'
+import SettingsPage from './pages/SettingsPage'
 import Layout from './components/Layout'
 
 function PrivateRoute({ children }) {
@@ -15,14 +17,17 @@ function PrivateRoute({ children }) {
 export default function App() {
   const { user } = useAuth()
   return (
-    <Routes>
-      <Route path="/login" element={user ? <Navigate to="/" /> : <LoginPage />} />
-      <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
-        <Route index element={<ListsPage />} />
-        <Route path="lista/:id" element={<ListDetailPage />} />
-        <Route path="productos" element={<ProductsPage />} />
-        <Route path="pendientes" element={<PendingPage />} />
-      </Route>
-    </Routes>
+    <ThemeProvider>
+      <Routes>
+        <Route path="/login" element={user ? <Navigate to="/" /> : <LoginPage />} />
+        <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
+          <Route index element={<ListsPage />} />
+          <Route path="lista/:id" element={<ListDetailPage />} />
+          <Route path="productos" element={<ProductsPage />} />
+          <Route path="pendientes" element={<PendingPage />} />
+          <Route path="configuracion" element={<SettingsPage />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   )
 }
